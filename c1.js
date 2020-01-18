78205e24a988f2245c9d5f55bf950747d9d18839
ygg <scyggdrasil@outlook.com>
2020-01-17 20:27:41 -0500

    Initial commit
    chapter 1 almost done ^-^

---
+++ c1.js
@@ -0,0 +1,71 @@
+revamp.chapters.push(
+{title: "Heuristic",
+number: 1,
+content: `"Mika, I can't leave, I can't!" Yuu clutched the fabric of Mika's shirt, trying to pull him along. "I finally have a family! I won't lose you!"
+
+Mika used the last of his strength to weakly shove at Yuu. "Just go, you idiot! Now!"
+
+At that moment, a short vampire with her hair in pink pigtails appeared in the doorway. She immediately took notice of Ferid lying on the floor with a gun wound in his head. "Oh my. How did these children catch you by surprise? You're a seventh progenitor for crying out loud."
+
+Ferid sat up and smiled smugly at her. "What a treat to be visited by our beloved Queen, Krul Tepes."
+
+"My seraphs were among these children. If you have any explanation for your actions, now would be the time to start."
+
+Desperately hoping the vampires were distracted, Yuu continued pulling at Mika, who no longer had enough strength to resist. They moved inch by inch toward potential escape while the nobles threatened each other - something about seraphs and councils, but Yuu couldn't be bothered to pay attention, until Ferid dramatically left the room and the queen was free to direct her disdainful glare elsewhere.
+
+"Now that that nuisance is dealt with..." Krul turned her attention toward the black haired boy who was trying and failing at subtly dragging a 90% dead boy out of Sanguinem. She used her vampiric speed to intercept them. "What do we have here?" She tilted her head, seeming to be contemplating something.
+
+Yuu's grip on Mika's shirt got tighter. "Leave us alone!"
+
+"I'm afraid I'd rather not. With a bit of my help, that boy is going to live." The queen bit her lip and leaned down toward the blond.
+
+"Get away from him! We don't need your help! We-"
+
+Yuu was knocked out by Krul to prevent him from screaming his head off at her. She then faced Mika. "Now, as for you, you will cease to be human, and become something superior."
+
+And with that, she sealed his fate with a kiss.
+
+-
+
+Mika awoke to bright lights and piercing silence.
+
+Usually when he first woke up, his surroundings were somewhat blurry, but this time, everything was sharp, too sharp. The stairs which must have been several meters away appeared as clearly as if they were at an arm's length, and this change in his depth perception was almost dizzying. He tried to focus in on any sound instead, but there was not even breathing and the complete lack of noise made his head hurt almost as much as his parched throat.
+
+Wait, but hadn't he died.
+
+And, wasn't he in so much pain before? He could remember it entirely, but now he felt better than ever.
+
+"Finally, you've become conscious."
+
+Mika winced at the high voice suddenly breaking the silence. He sat up to see that he had been lying on the red carpet of a large room.
+
+Krul was sitting on a throne at the top of the stairs, leaning majestically to one side. "I requested that you be brought here once the first part of the transformation completed."
+
+"Transformation...?"
+
+The queen picked up a cup from her armrest and tilted it back and forth, and Mika watched the red liquid inside slosh tantalizingly.
+
+Then it hit him. The reason he could see the drink from so far away, why neither he nor Krul was breathing by default, why he was so /thirsty/.
+
+He was not dead. He was worse than dead.
+
+(Mention Yuu is captured. Krul tempts Mika with blood.)
+
+"But, perhaps you need more persuasion."
+
+One of the tall doors was opened by a vampire in uniform and a small figure was led into the room. It was Yuu with a distinct lack of livestock collar, and eerily calm. Mika's gaze focused suddenly on the bare skin of Yuu's neck and his breath caught in his dry, aching throat. "I..."
+
+"They told me I can save you if I give you my blood." Yuu raised his gaze from the floor and stared into Mika's eyes bravely.
+
+Mika took a step back and shook his head, distressed. "No. I won't do it!"
+
+"Mika! You can't starve yourself. Or I'll...I'll-!" He couldn't think of what to say. "I almost lost you once already!"
+
+"Look at me, Yuu-chan! I'm already becoming a monster. How could you say..." Mika clenched his fists weakly as if trying to hold onto the little humanity he had left. "Please. Don't ask me to do this."
+
+"Hmm...an interesting development." Pondering the situation, Krul rested her head on the back of her hand. "A semi-vampire who wishes to remain as such, and a human who would do anything for him..." She turned back to face the newly sired vampire again. "So, you desire to live among humans without consuming their blood? I suppose that could be arranged."
+
+Both boys' wide eyes were on her in a split second.
+
+"How would the both of you like to become my spies?"`}
+)
---
+++ c4.js
@@ -0,0 +1,39 @@
+revamp.chapters.push(
+{title: "The Glass Break Alarm, Reprise",
+number: 4,
+content: `"Are you sure the drink is punch?"
+
+Mika nodded carefully though he felt worried. He swallowed, his throat still craving the blood.
+
+"You got punch!" Yuu had just barged into the room and his eyes sparkled. He snatched the cup from where Guren had recently placed it back.
+
+"Wait no Yuu-chan-" Mika lunged for it, but Yuu already was sipping the red liquid.
+
+Yuu coughed and made a face. "Mika, this-" He stopped because he saw Mika's terrified expression.
+
+The metaphorical gears in Mika's head were spinning doubly fast. "So! If that's all you wanted to ask, we'll just...get going here!" He stumbled over his words and also over his feet in his attempt to exit the room, Yuu's hand firmly in his grasp.
+
+Yuu's face was pale. "Mika, I feel..."
+
+Mika's throat seemed dry but not from lack of blood. "Yuu-chan...you didn't /swallow/ the blood, did you?"
+
+"Just a little! It'll be fine!" He swayed slightly in place.
+
+For a second, Mika reviewed his entire existence. "We need to get you to our room, now."
+
+He helped Yuu who leaned stumbling.
+
+-Mika manages to get Yuu in but before Yuu starts screaming. He buries Yuu in pillows (Vamps don't need to breathe and he needs to stifle the screams)
+
+In a panic, Mika smothered Yuu in blankets. Vampires didn't need to breathe so it would be fine.
+
+But the glass break alarm goes off
+
+Mika rushes into the hall with a lamp and throws it on the floor. He starts crying. People show up and take Mika to get cleaned up. They ignore the music in the room. Mika's cut heals too fast so he's like "Guess small cuts can bleed a lot" he crushed the lamp before throwing it.
+
+"I...wanted to get it replaced, but..."
+
+Mika returned to the room as quickly as he could without causing suspicion. Inside, he shut the door and flopped down on the bed. The adrenaline now disappearing, Mika felt the weight of what was happening crashing down on him. Yuu-chan was going to become a monster, too...
+
+Real tears started to prick at his eyes and he wrapped his arms around the trembling bundle of blankets "I'm sorry, Yuu-chan"`}
+)

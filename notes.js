f086a216b6257eef6089a7f1dabb1f54b6c9821b
ygg <scyggdrasil@outlook.com>
2020-01-31 13:19:18 -0500

    notes replacement

---
+++ notes.js
@@ -0,0 +1,234 @@
+revamp.chapters.push({title:"Notes",number:-1,privacy:2,content:
+`(Revamp) Vampires? In <i>my</i> JIDA? It's more likely than you think
+
+• Food, housing, and arukanu stationery
+
+• Follow you into the dark
+
+• Heuristic
+
+• Broken glass/the glass break alarm (reprise)
+
+-
+
+With a cry, he hurled the bocks across the room into the corner. Some of the vials landed on the soft comforter of his bed, while others thumped onto the plush carpet and beanbag chairs, much to his dismay. All of the vials [were | remained] intact.
+
+-
+
+"Are you sure the drink is punch"
+
+Mika nodded carefully while feeling worried. He swallowed, his throat still craving the blood.
+
+"You got punch!" Yuu had just barged into the room and his eyes [glittered | sparkled]. He snatched the cup from where Guren had just placed it back.
+
+Mika lunged for it "Wait no Yuu-chan"
+
+Yuu already was [drinking | sipping] the red liquid. He coughed and made a face "Mika, this is" He stopped because he saw Mika's terrified expression.
+
+-
+
+The tapping on the window was getting a slight bit annoying so he unlatched it. In came an odd bat creature that was carrying a box in its claws.
+
+"To: Mikaela From: Krul Tepes, Vampire Queen"
+
+-
+
+#Yuu's face was pale. "Mika, I feel..."
+
+#Mika's throat seemed dry but not from lack of blood. "Yuu-chan...you didn't <i>swallow</i> the blood, did you"
+
+#"Just a little! It'll be fine!" He swayed slightly in place.
+
+#"We need to get you to our room, now." Mika was filled with panic. He helped Yuu who leaned stumbling.
+
+#-
+
+[Mika manages to get Yuu in but before Yuu starts screaming. He buries Yuu in pillows (Vamps don't need to breathe and he needs to stifle the screams) But the glass break alarm goes off]
+
+Mika rushes into the hall with a lamp and throws it on the floor. He starts crying. People show up and take Mika to get cleaned up. They ignore the music in the room. Mika's cut heals too fast so he's like "Guess small cuts can bleed a lot" he crushed the lamp before throwing it.
+
+-
+
+"Dearest Mika," Krul sends notes with Arukanu. Mika opened the box. It contained vials of blood, UV guards, and a pack of Arukanu stationery.
+
+The creature bumped the window. "I don't want it, go away!" Arukanu drew back and crashed into...
+
+"The sun...it hurts..." Mika curled further into a ball.
+
+Who knew how it worked, but it did. [referring to the uv guards]
+
+[Krul sends Mika as a spy since he's depending on her blood.]
+
+After Arukanu breaks the window, the glass break alarm goes off again. Yuu jumps out the window and throws a rock into the room. People arrive as Yuu starts apologizing. Arukanu has flown off and Mika has to quickly hide the package
+
+In a panic, Mika smothered Yuu in blankets. Vampires didn't need to breathe so it would be fine.
+
+"I...wanted to get it replaced, but..."
+
+-
+
+Mika returned to the room as quickly as he could without causing suspicion. Inside, he shut the door and flopped down on the bed. The adrenaline now disappearing, Mika felt the weight of what was happening crashing down on him. Yuu-chan was going to become a monster, too...
+
+Real tears started to prick at his eyes and he wrapped his arms around the trembling bundle of blankets "I'm sorry, Yuu-chan"
+
+-
+
+Yuu emerged from the blankets like a butterfly from a cocoon. Except, this wasn't a metamorphosis meant for him in Mika's opinion.
+
+"Not enough blood for a growing vampire" (to Krul)
+
+Why Krul sends Mika to JIDA
+
+• Mika doesn't want to be a full vampire, so he is dependent on her
+
+• He grows like a human
+
+• His eyes aren't red
+
+• She knows he's savvy and resourceful
+
+"I can't make a contract with you. You're not human." The demon turned away with his hands behind his back.
+
+(Yuu leaves) (He grumbles and Mika based on his knowledge of business says "Anyone can make a contract with anyone, humans do it among themselves all the time") [then angsts because he's not human. RIP Mika]
+
+"Dear Queen Krul
+
+Please send more UV protection bands.
+
+The first one you gave me did not survive being caught on a branch when I fell out of the tree.
+
+Sincerely, Mikaela"
+
+-
+
+Just then Guren walked by them and Yuu's eyes glittered in admiration. "When I grow up I want to be just like him"
+
+Mika put his hand to his forehead and sighed. "Please don't."
+
+#-
+
+#"But, perhaps you need more persuasion." It was Yuu with a distinct lack of livestock collar. Mika's gaze focused immediately on the bare skin of Yuu's neck and his breath caught in his dry, aching throat. "I..."
+
+#"They told me I can save you (from becoming a demon)? if..."
+
+#(Yuu yelling about not leaving family) (Krul appears and sends Ferid away in anger) "Now that that nuisance is dealt with..." She turned her attention toward the black haired boy who was trying and failing at subtly dragging a 90% dead boy out of Sanguinem. Krul used her vampiric speed to intercept them.
+"Oh? What do we have here?"
+
+#Yuu's grip on Mika's shirt got tighter. "Leave us alone!"
+
+#"Do not be afraid. I am going to save him."
+
+#The queen bit her lip and leaned down toward the blond.
+
+#"Get away from him! We don't need your help! We-"
+
+#Yuu was knocked out by Krul so he would stop [to prevent him from] screaming his head off.
+
+#"Now, you will cease to be human."
+
+#And with that, she sealed his fate with a kiss.
+
+#"Hmm...an interesting development [is this stolen from cc]. Krul rested her head on the back of her hand. "A human who would do anything for him..." She turned back to face them again. "How would the both of you like to become my spies?"
+
+(Main plot points) Spying on the authorities about the Seraph experiments, Guren being suspicious of Mika, trying to hide vamp characteristics, Yuu becoming a vamp, contract with Asuramaru, JIDA training
+
+-
+
+<strong>Random blood sharing stuff?</strong>
+
+Warm, salty liquid filled his mouth. Blood reheated from the microwave could not begin to rival this taste of fresh food. He faintly could hear Yuu sigh.
+
+Still licking the [residue | residual taste] off his fangs
+
+"Ahh...it didn't hurt too much, did it?"
+
+"No, actually...it felt really, really nice." Yuu touched his neck and brought his gaze up to meet Mika's (own).
+
+Mika's back hit the bed softly. Yuu was still nuzzled (nestled?) into his neck. "Can I drink from you, too?"
+
+He shivered as Yuu's fangs grazed his neck.
+
+It was like a wave, a cool rush through his body, followed by a tingling feeling.
+
+But his thoughts were quickly lost as his mind started to feel filled with clouds [Careful with mimicking guidebook too much].
+
+-
+
+"Don't make a sound." Mika tightened his grip on Yuu's wrists, pinning him down on the beanbag chairs. Yuu had an expression of bewilderment on his face.
+
+[The sound again, louder.]
+
+Silently they both [looked | turned to look] toward the source of the sound (noise).
+
+-
+
+Way to stick it to her, Boss Man
+
+#-
+
+#"Ahh! But you have plenty of explaining to do yourself, don't you my Queen?" Ferid's grin was smug. "If the Progenitor Council knew you were fiddling with the seraphs, they'd be furious."
+
+#"Hmm, what did you say?"
+
+#"You heard me, if I told the Progenitor Council-"
+
+#Smack. Krul's kick connected with Ferid's chest and sent him flying into a wall. She stood over him with a foot on his neck to keep him down. "I'm sorry, I didn't catch what you were saying."
+
+#"You're so violent...Alright. My lips are sealed when it comes to your precious seraphs."
+
+#Krul narrowed her eyes. "Good. They'd better be. Because if you pry into this matter again, you'll regret it."
+
+#"Don't worry, I value my life far too much to do that. But you haven't seen the last of me, Krul!"
+
+#Ferid dramatically left the room under the queen's cold stare.
+
+-
+
+"Stand back, Mika, I got this." Yuu faced the horseman head-on, one hand clutching his bright red weapon to his chest, the other poised to pull the trigger. "While you were reading boring legal stuff, I studied the blade."
+
+Mika stared in disbelief. "That's not even a blade."
+
+"Yeah, well the army's gonna wish they used <em>these</em> when they see this epic take down." Yuu smirked, brandishing the fire extinguisher.
+
+-
+
+"He just has very sharp teeth I guess"
+
+"Thanks, you're an angel."
+
+"Wish I wasn't"
+
+Mika wrinkled his nose. Someone was burning popcorn downstairs again.
+
+Yuu gets a beanie later
+
+-
+
+the idea that a pair of 12 year olds somehow end thousands of years of human experimentation is delightful
+
+[deus ex machima]
+
+[put literal god in it]
+
+sun god aten yuu
+
+[they settle it with a duel]
+
+definitely there needs to be a really overdramatic ending battle
+
+probably with shikama doji
+
+maybe involving guren, I don't know
+
+then it can be all sparkly and magic and happy
+
+and yuu kisses mika's cheek and says something dumb like
+
+[pls kill guren with the spoon]
+
+"we're gonna get a house and all the seraph kids can live there. and we can eat curry and strawberry cake, and date each other when we're 13."
+
+-
+
+(defiant is a good word, scrutiny for guren, fastened)
+`})
